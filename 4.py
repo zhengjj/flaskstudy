578bcfce5c3200a79cd0fd6378ce4afd6ca11f31
zhengjj <zjj1@163.com>
2021-12-03 15:59:24 +0800

    第4课习题

--- 4.py
+++ 4.py
@@ -4,4 +4,21 @@
 # @Time : 2021/12/02 16:56:04
 #
 
-import 
+from os import name
+from flask import Flask,render_template_string
+from flask.templating import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    books={'bookname':'test01','bookauthor':'my','pages':99}
+    return render_template('4.html',**locals())
+
+@app.route('/99/')
+def cf99():
+    num = range(1,10)
+    return render_template('5.html',**locals())
+
+if __name__=='__main__':
+    app.run(port=8080)
